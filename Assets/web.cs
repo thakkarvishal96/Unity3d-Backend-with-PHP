a9f28c32dbba1911bac0e6b04309c8fb4993d038
vishal dev <vishal.unity3d@gmail.com>
2020-04-27 13:10:08 +0530

    1st

---
+++ Assets/web.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+using System.Collections;
+
+public class web : MonoBehaviour
+{
+    void Start()
+    {
+        // A correct website page.
+        StartCoroutine(Gettext());
+
+    }
+
+    IEnumerator Gettext() 
+    {
+        using (UnityWebRequest WWW = UnityWebRequest.Get("http://localhost/unity/Userdata.php"))
+        {
+            // Request and wait for the desired page.
+            yield return WWW.Send();
+
+
+            if (WWW.isNetworkError || WWW.isHttpError)
+            {
+                Debug.Log(": Error: " + WWW.error);
+            }
+            else
+            {
+                Debug.Log(":\nReceived: " + WWW.downloadHandler.text);
+
+                byte[] results = WWW.downloadHandler.data;
+            }
+        }
+    }
+}
